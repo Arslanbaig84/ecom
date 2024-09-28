2a4aeb7be7267c9baf4bd0200a1700f838469079
Arslanbaig84 <arslan.multiline@gmail.com>
2024-09-28 06:19:12 +0500

    edited admin access for users to include more fields

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,12 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name')
+        fields = ('email', 'first_name', 'last_name', 'contact', 'address', )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name')
+        fields = ('email', 'first_name', 'last_name', 'contact', 'address')
