f6be73f55944dd7335662bee2923171157423bff
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-03 06:13:11 +0500

    cart and cartitem models

--- transactions/models.py
+++ transactions/models.py
@@ -4,7 +4,7 @@ from users.models import CustomUser
 from products.models import Product
 
 # Create your models here.
-def Cart(BaseModel):
+class Cart(BaseModel):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='cart')
     is_active = models.BooleanField(default=True)
 
@@ -14,7 +14,7 @@ def Cart(BaseModel):
     def total_price(self):
         return sum(item.price() for item in cart_items.price.all())    
 
-def CartItem(BaseModel):
+class CartItem(BaseModel):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_items')
     quantity = models.PositiveSmallIntegerField()
