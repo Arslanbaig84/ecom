220c5c707fa180021ae7a7d3fc1397b8761d5f1f
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-03 17:06:40 +0500

    completed add_to_cart function

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('add_to_cart/<slug:product_slug>', views.add_to_cart, name='add_to_cart'),
     path('cart', views.cart, name='cart')
 ]
